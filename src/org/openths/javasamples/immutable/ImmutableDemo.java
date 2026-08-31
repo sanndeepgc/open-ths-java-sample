70101c54b010ae3219ee197093f30b25da44c8b8
Saniya Samrin D <samrindsaniya@gmail.com>
2026-08-31 16:16:56 +0530

    [2] : Develop an example for custom immutable object demonstration in Java

--- src/org/openths/javasamples/immutable/ImmutableDemo.java
+++ src/org/openths/javasamples/immutable/ImmutableDemo.java
@@ -4,13 +4,12 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 public class ImmutableDemo {
-     static  void main(String[] args) {
+    static void main(String[] args) {
         Invoice invoice = new Invoice("SSD8989", 989, LocalDateTime.now(), new Date());
-//
-         System.out.println("Initial Date : " + invoice);
+        System.out.println("Initial Date : " + invoice);
         Date invoiceCreatedDate = invoice.getCreatedDate();
         invoiceCreatedDate.setDate(29);
-         invoiceCreatedDate.setYear(2000);
+        invoiceCreatedDate.setYear(2000);
 
         System.out.println("Post modifying the created date field : " + invoice);
     }
