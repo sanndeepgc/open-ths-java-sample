c409897088140aebd825d4fea99b4cc70ead4969
Sandeep G <sandeep.g@beamteq.com>
2026-09-11 14:49:11 +0530

    Add Concurrency logics to run multiple launches on the mission controls.

--- src/org/openths/javasamples/j25/features/scoped/MissonControlDemo.java
+++ src/org/openths/javasamples/j25/features/scoped/MissonControlDemo.java
@@ -1,27 +1,75 @@
 package org.openths.javasamples.j25.features.scoped;
 
+import java.util.concurrent.CompletableFuture;
+
 public class MissonControlDemo {
-    void main(){
+  void main() {
 
-        MissionControl missionControl = new MissionControl("INDIA","ISRO");
+    MissionControl indianMissionControl = new MissionControl("INDIA", "ISRO");
+    MissionControl ussrMissionControl = new MissionControl("USSR", "TsUP");
+    MissionControl usaMissionControl = new MissionControl("USA", "MCC-H");
 
-        ScopedValue.where(MissionControl.CLEARANCE, "PUBLIC").run(() ->{
-            missionControl.launchRocket(5000);
-        });
+    CompletableFuture<Void> usaFuture =
+        CompletableFuture.runAsync(
+                () -> {
+                  ScopedValue.where(usaMissionControl.CLEARANCE, "TOP SECRET")
+                      .run(
+                          () -> {
+                            System.out.println("USA MCC-H  clearance will be ready in 5 seconds.");
+                            try {
+                              Thread.sleep(5000);
+                              usaMissionControl.launchRocket("USA-TSEC-1", 10000);
 
-        ScopedValue.where(MissionControl.CLEARANCE, "CONFIDENTIAL").run(() ->{
-            missionControl.launchRocket(1000);
-        });
+                            } catch (InterruptedException e) {
+                              throw new RuntimeException(e);
+                            }
+                          });
+                })
+            .thenRun(
+                () -> {
+                  System.out.println("usaMissionControl is complete");
+                });
 
-        System.out.println(MissionControl.CLEARANCE.orElse("No launches"));
+    CompletableFuture<Void> indiaFuture =
+        CompletableFuture.runAsync(
+                () -> {
+                  ScopedValue.where(indianMissionControl.CLEARANCE, "PUBLIC")
+                      .run(
+                          () -> {
+                            indianMissionControl.launchRocket("MISSION-MANGAL", 5000);
+                          });
+                  ScopedValue.where(indianMissionControl.CLEARANCE, "SECRET")
+                      .run(
+                          () -> {
+                            indianMissionControl.launchRocket("CHANDRAYAAN-I", 25000);
+                          });
+                  ScopedValue.where(indianMissionControl.CLEARANCE, "SECRET")
+                      .run(
+                          () -> {
+                            indianMissionControl.launchRocket("CHANDRAYAAN-II", 25000);
+                          });
+                })
+            .thenRun(
+                () -> {
+                  System.out.println(
+                      String.format(
+                          "%s is complete", indianMissionControl.getMissionControlName()));
+                });
 
-        ScopedValue.where(MissionControl.CLEARANCE, "SECRET").run(() ->{
-            missionControl.launchRocket(2000);
-        });
+    CompletableFuture<Void> ussrFuture =
+        CompletableFuture.runAsync(
+                () -> {
+                  ScopedValue.where(ussrMissionControl.CLEARANCE, "USSR PUBLIC")
+                      .run(
+                          () -> {
+                            ussrMissionControl.launchRocket("CONFIDENTIAL-01", 5000);
+                          });
+                })
+            .thenRun(
+                () -> {
+                  System.out.println("ussrMissionControl is complete");
+                });
 
-        ScopedValue.where(MissionControl.CLEARANCE, "TOP_SECRET").run(() ->{
-            missionControl.launchRocket(500);
-        });
-
-    }
+    CompletableFuture.allOf(ussrFuture, usaFuture, indiaFuture).join();
+  }
 }
